dfa09f33f0dadd864717d0c96fdedaa695fb1702
John Grishin <r@exah.ru>
2017-03-03 01:28:37 +0300

    Move from dist to lib

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   output: {
     filename: '[name].js',
     library: 'funAnimate',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'lib')
   },
   module: {
     rules: [
