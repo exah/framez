d4221e649f4a8870aabe63c44412d23992094ddb
John Grishin <r@exah.ru>
2017-03-03 01:24:27 +0300

    Build with webpack

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import animate from './animate'
+import * as handlers from './handlers'
+import * as easing from './easing'
+
+export { handlers, easing, animate }
+
+export default animate
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path')
+
+module.exports = {
+  entry: {
+    main: './src/index.js',
+    animate: './src/animate.js',
+    easing: './src/easing.js',
+    handlers: './src/handlers.js'
+  },
+  output: {
+    filename: '[name].js',
+    library: 'funAnimate',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  }
+}
