d7c0dea35816387df9dedccd56b6487a369d700d
John Grishin <r@exah.ru>
2017-03-22 15:11:41 +0300

    Fix easings

--- src/easings.js
+++ src/easings.js
@@ -1,26 +1,29 @@
-// Copy-pasted from comments
-// https://gist.github.com/gre/1650294
+// Copy-pasted from https://gist.github.com/gre/1650294
 
-export const powerEaseIn = (power) => (t) => Math.pow(t, power)
-export const powerEaseOut = (power) => (t) => 1 - Math.abs(Math.pow(t - 1, power))
-export const powerEaseInOut = (power) => (t) => (
-  t < 0.5
-    ? powerEaseIn(power)(t * 2) / 2
-    : powerEaseOut(power)(((t * 2) - 1) / 2) + 0.5
-)
+export const easeInQuad = (t) => t * t
+// decelerating to zero velocity
+export const easeOutQuad = (t) => t * (2 - t)
+// acceleration until halfway, then deceleration
+export const easeInOutQuad = (t) => t < 0.5 ? 2 * t * t : -1 + ((4 - (2 * t)) * t)
+// accelerating from zero velocity
+export const easeInCubic = (t) => t * t * t
+// decelerating to zero velocity
+export const easeOutCubic = (t) => ((--t) * (t * t)) + 1
+// acceleration until halfway, then deceleration
+export const easeInOutCubic = (t) => t < 0.5 ? 4 * t * t * t : ((t - 1) * ((2 * t) - 2) * ((2 * t) - 2)) + 1
+// accelerating from zero velocity
+export const easeInQuart = (t) => t * t * t * t
+// decelerating to zero velocity
+export const easeOutQuart = (t) => (1 - (--t)) * (t * (t * t))
+// acceleration until halfway, then deceleration
+export const easeInOutQuart = (t) => t < 0.5 ? 8 * t * t * t * t : 1 - (8 * (--t) * t * t * t)
+// accelerating from zero velocity
+export const easeInQuint = (t) => t * t * t * t * t
+// decelerating to zero velocity
+export const easeOutQuint = (t) => 1 + ((--t) * t * t * t * t)
+// acceleration until halfway, then deceleration
+export const easeInOutQuint = (t) => t < 0.5 ? 16 * t * t * t * t * t : 1 + (16 * (--t) * t * t * t * t)
 
-export const easeInQuad = powerEaseIn(2)
-export const easeOutQuad = powerEaseOut(2)
-export const easeInOutQuad = (t) => (t < 0.5) ? 2 * t * t : -1 + ((4 - (2 * t)) * t)
-export const easeInCubic = powerEaseIn(3)
-export const easeOutCubic = powerEaseOut(3)
-export const easeInOutCubic = powerEaseInOut(3)
-export const easeInQuart = powerEaseIn(4)
-export const easeOutQuart = powerEaseOut(4)
-export const easeInOutQuart = powerEaseInOut(4)
-export const easeInQuint = powerEaseIn(5)
-export const easeOutQuint = powerEaseOut(5)
-export const easeInOutQuint = powerEaseInOut(5)
 export const easeInSin = (t) => 1 + Math.sin((Math.PI / 2 * t) - (Math.PI / 2))
 export const easeOutSin = (t) => Math.sin(Math.PI / 2 * t)
 export const easeInOutSin = (t) => (1 + Math.sin((Math.PI * t) - (Math.PI / 2))) / 2
