ab0e5b338743eab85d8630b741f6a83fc1b649ee
John Grishin <r@exah.ru>
2017-03-28 00:48:26 +0300

    Add getUnit, getNumber, nextUnit utility function

--- src/utils.js
+++ src/utils.js
@@ -11,3 +11,23 @@ export const compose = (fns) => (initial) => fns.reduce((value, fn) => fn(value)
 
 export const nextNumber = (start, end, progress) => ((end * progress) + start)
 
+//
+// Parse number with unit
+// 50px -> [ 50, 'px' ]
+//
+
+export const getNumber = (str) => parseFloat(str, 10)
+export const getUnit = (str) => String(str).replace(/([0-9]|\.|,)+([\S]+)?/, '$2').trim()
+
+//
+// Get next unit number using nextNumber
+//
+
+export const nextUnit = (startUnit, endUnit) => {
+  const unit = getUnit(startUnit) || getUnit(endUnit) || 0
+  const start = getNumber(startUnit)
+  const end = getNumber(endUnit)
+
+  return (progress) => nextNumber(start, end, progress) + unit
+}
+
