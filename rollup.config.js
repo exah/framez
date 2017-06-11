27533275dd15bcf92df99c81b4cc844b4f7373fc
John Grishin <r@exah.ru>
2017-06-11 09:04:27 +0300

    Update umd name and package.json

--- rollup.config.js
+++ rollup.config.js
@@ -5,14 +5,14 @@ import uglify from 'rollup-plugin-uglify'
 
 export default {
   entry: 'src/index.js',
+  dest: 'dist/index.umd.js',
   format: 'umd',
   moduleName: 'A',
+  sourceMap: true,
   plugins: [
     json(),
     resolve(),
     buble({ objectAssign: 'Object.assign' }),
     uglify()
-  ],
-  sourceMap: true,
-  dest: 'dist/a.umd.bundle.js'
+  ]
 }
