db0a136af12ce807bcfba8aa05df3ef037456494
John Grishin <r@exah.ru>
2017-06-11 21:20:16 +0300

    Compile `core` and `easings` as umd modules

--- rollup.config.js
+++ rollup.config.js
@@ -3,11 +3,11 @@ import babili from 'rollup-plugin-babili'
 import json from 'rollup-plugin-json'
 import resolve from 'rollup-plugin-node-resolve'
 
-export default {
-  entry: 'src/index.js',
-  dest: 'dist/index.umd.js',
+const config = (entry, dest, moduleName) => ({
+  entry,
+  dest,
+  moduleName,
   format: 'umd',
-  moduleName: 'A',
   sourceMap: true,
   plugins: [
     json(),
@@ -15,4 +15,10 @@ export default {
     buble({ objectAssign: 'Object.assign' }),
     babili({ comments: false })
   ]
-}
+})
+
+export default [
+  config('src/index.js', 'dist/index.umd.js', 'framez'),
+  config('src/core/index.js', 'dist/core/index.umd.js', 'framezCore'),
+  config('src/easings/index.js', 'dist/easings/index.umd.js', 'framezEasings')
+]
