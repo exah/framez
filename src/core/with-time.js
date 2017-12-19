6e147572bbd38c8aa6c9b28640823c1447d7e90a
John Grishin <r@exah.me>
2017-12-19 20:08:17 +0300

    ✨ Pass withTime state to handlers

--- src/core/with-time.js
+++ src/core/with-time.js
@@ -17,68 +17,65 @@ function withTime (optsOrDuration = {}) {
   const { duration, lastFrame, direction, loop } = opts
   const maxIterations = opts.iterations || (loop ? Infinity : direction === 'alternate' ? 2 : 1)
 
-  let iteration = 0
-  let start = null
-  let end = null
-
   const adjustProgress = (progress, isReversed) => (
     isReversed ? MAX_PROGRESS - progress : progress
   )
 
-  return (result = {}) => {
-    const { now, play, stop, started } = result
+  return (res = {}) => {
+    const { now, play, stop, started } = res
+    res.iteration = res.iteration || 0
 
-    const isStart = (start == null || end == null)
+    const isStart = (res.start == null || res.end == null)
     if (isStart) {
-      start = now
-      end = (now + duration)
-      started.resolve()
+      res.start = now
+      res.end = (now + duration)
+      started.resolve({ ...res })
     }
 
     const isReversed = (
       (direction === 'reverse') ||
-      (direction === 'alternate' && iteration % 2 !== 0)
+      (direction === 'alternate' && res.iteration % 2 !== 0)
     )
-    const elapsed = (now - start)
-    const remain = (end - now)
+    const elapsed = (now - res.start)
+    const remain = (res.end - now)
     const progress = (elapsed / duration)
 
     const isEnd = (remain < lastFrame)
     if (isEnd) {
-      start = null
-      end = null
+      res.start = null
+      res.end = null
 
-      const nextIteration = (iteration + 1)
+      const nextIteration = (res.iteration + 1)
       if (nextIteration < maxIterations) {
-        iteration = nextIteration
+        res.iteration = nextIteration
         play()
       } else {
-        iteration = 0
+        res.iteration = 0
         stop()
       }
 
       return {
+        ...res,
+        progress: adjustProgress(MAX_PROGRESS, isReversed),
         elapsed: duration,
         remain: 0,
-        progress: adjustProgress(MAX_PROGRESS, isReversed),
         duration,
         isReversed,
         isStart,
-        isEnd,
-        ...result
+        isEnd
       }
     }
 
     play()
     return {
+      ...res,
       progress: adjustProgress(progress, isReversed),
       elapsed,
       remain,
       duration,
       isReversed,
       isStart,
-      isEnd,
-      ...result
+      isEnd
     }
   }
 }
