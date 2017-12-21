6a218fb0574070011949a2e89bfa61e745f990ab
John Grishin <r@exah.ru>
2017-12-22 01:07:01 +0300

    🚨 Fix indentation

--- src/easings/elastic-in.js
+++ src/easings/elastic-in.js
@@ -2,7 +2,7 @@ const elasticIn = (power = 1) =>
   (t) => (t === 0 || t === 1)
     ? t
     : -Math.pow(2, 10 * (t - 1)) * Math.sin(
-        (((t - 1) - (power / (Math.PI * 2) * Math.asin(1))) * (Math.PI * 2)) / power
-      )
+      (((t - 1) - (power / (Math.PI * 2) * Math.asin(1))) * (Math.PI * 2)) / power
+    )
 
 export default elasticIn
